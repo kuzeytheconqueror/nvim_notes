fddddc6274bf8d20e2dcb1aea6df5426df9eaaa1
samliumay <samliumay965@gmail.com>
2026-09-10 14:53:14 +0300

    just a basic express server to try some new stuff.

--- trials/express-api/server.js
+++ trials/express-api/server.js
@@ -5,27 +5,44 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-list = {};
+const list = [];
+
+let aut_id = 1;
 
 app.get("/api/hello", (req, res) => {
 	res.json({ message: "Hello, world!" });
-	console.log("message came!");
+	console.log("Hello world camed");
+});
+
+app.get("/api/greet", (req, res) => {	
+	res.json({ message: "greetings!" });
+	console.log("greeting camed!");
 });
 
 app.post("/api/post", (req, res) => {
 
-	const {name, email} = req.json();
+	const {name, email} = req.body;
 
 
 	if(!name || !email){
 	return res.status(400).json({error: "Name and email is required"}); 
 	}
 
-	const newuser = { id: 1, name, email}
+	const newuser = { id: aut_id, name, email};
+	
+	list.push(newuser);
+
+	aut_id++;
 
 	res.status(201).json(newuser);
+});
+
+app.get("/api/getall", (req, res) => {
+	res.json(list);
 });
 
 app.listen(port, () => {
 	console.log(`Server running at http://localhost:${port}`);
 });
+
+
