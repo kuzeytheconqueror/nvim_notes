23e1b2085f53393edd6a73c335fad41d4a2f388b
samliumay <samliumay965@gmail.com>
2026-09-10 11:57:55 +0300

    nvim first tutorial done.

--- trials/express-api/server.js
+++ trials/express-api/server.js
@@ -5,9 +5,25 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+list = {};
+
 app.get("/api/hello", (req, res) => {
 	res.json({ message: "Hello, world!" });
 	console.log("message came!");
+});
+
+app.post("/api/post", (req, res) => {
+
+	const {name, email} = req.json();
+
+
+	if(!name || !email){
+	return res.status(400).json({error: "Name and email is required"}); 
+	}
+
+	const newuser = { id: 1, name, email}
+
+	res.status(201).json(newuser);
 });
 
 app.listen(port, () => {
